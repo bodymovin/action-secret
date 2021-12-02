73a6feb537bfdccadaeaabbd26730bdc1a1c88fd
airnan <hernantorrisi@gmail.com>
2021-12-02 08:55:12 -0500

    logging secret test

--- dist/index.js
+++ dist/index.js
@@ -8449,7 +8449,9 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello 2 ${nameToGreet}!`);
+  const printSecret = process.env.SECRET_TEST + '_TEST';
   console.log(process.env.SECRET_TEST);
+  console.log(printSecret);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
