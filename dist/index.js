c41069326e89f1edee29537204a73f70aef78d54
airnan <hernantorrisi@gmail.com>
2021-12-02 08:29:32 -0500

    added env vars

--- dist/index.js
+++ dist/index.js
@@ -8449,6 +8449,8 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello 2 ${nameToGreet}!`);
+  console.log(process.env);
+  console.log(process.env.FIRST_NAME);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
