57fff5b39104fb3c51f0bb939cca313f23e45841
airnan <hernantorrisi@gmail.com>
2021-12-02 08:32:31 -0500

    logging secret test

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello 2 ${nameToGreet}!`);
-  console.log(process.env);
-  console.log(process.env.FIRST_NAME);
+  console.log(process.env.SECRET_TEST);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
